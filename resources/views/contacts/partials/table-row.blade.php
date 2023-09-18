f207da7b77bf8c46c4565d552ca2fe127a1173e3
Muhammad Shafeeq <6695370+hmshafeeq@users.noreply.github.com>
2023-09-18 10:55:17 +0500

    wip

--- resources/views/contacts/partials/table-row.blade.php
+++ resources/views/contacts/partials/table-row.blade.php
@@ -1,19 +1,21 @@
-<tr id="contact-{{ $contact->id }}"
-    @if (request()->server('HTTP_HX_REQUEST') && request()->isMethod('put'))
-        hx-swap-oob="innerHTML:#contact-{{ $contact->id }}"
-    @endif
->
+<tr id="contact-{{ $contact->id }}">
     <td class="px-4 py-2 border">{{ $contact->name }}</td>
     <td class="px-4 py-2 border">{{ $contact->email }}</td>
     <td class="px-4 py-2 border">{{ $contact->phone }}</td>
     <td class="px-4 py-2 border">{{ $contact->address }}</td>
     <td class="px-4 py-2 border ">
-        <a hx-get="{{ route('contacts.show', $contact->id) }}" hx-target="#section" class="mr-1 uppercase hover:underline">View</a>
-        <a hx-get="{{ route('contacts.edit', $contact->id) }}" hx-target="#section" class="mr-1 uppercase hover:underline">Edit</a>
-        <a class="mr-1 uppercase hover:underline" hx-delete="{{ route('contacts.destroy', $contact->id) }}"
+        <a class="mr-1 uppercase hover:underline"
+           hx-get="{{ route('contacts.show', $contact->id) }}"
+           hx-target="#section">View</a>
+
+        <a class="mr-1 uppercase hover:underline"
+           hx-get="{{ route('contacts.edit', $contact->id) }}"
+           hx-target="#section">Edit</a>
+
+        <a class="mr-1 uppercase hover:underline"
+           hx-delete="{{ route('contacts.destroy', $contact->id) }}"
            hx-confirm="Are you sure you want to delete this contact?"
-           hx-headers='{"X-CSRF-TOKEN": "{{ csrf_token() }}"}'
-           hx-target="closest tr" hx-swap="outerHTML swap:400ms">Delete</a>
+           hx-headers='{"X-CSRF-TOKEN": "{{ csrf_token() }}"}'>Delete</a>
     </td>
 </tr>
 
